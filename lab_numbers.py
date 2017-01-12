cee2689ccb3afe13c791f45cd70f980f83e93dbe
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2017-01-11 23:48:48 -0500

    lab_numbers.py has been debugged
    This files has been partially debugged, currently still trying to
    ensure the results are the correct ones.

--- lab_numbers.py
+++ lab_numbers.py
@@ -5,27 +5,27 @@
 # Assignment: Numbers
 # All Rights Reserved
 
-#The program should print a short introduction about its purpose.
-print('This program calculate the square root of a given number using the Newton's method')
+#import math library
+import math
 
-#Prompt the user for the value xx to find x√x.
-x = input('What is the number for which you want to calculate the square root?')
-
-
-#Prompt the user for the number of times to improve the guess.
-n = input('How many iterations do you want to use?')
-
-#Using Newtwon's method in order to estimate the square root
-def newtownMethod(x,n):
-    guessValue = 0.5 * x #Starting guess value of (x/2)
-    for i in range(n):
-        finalValueOfGuess = 0.5 * (guessValue + x/ guessValue)
-        guessValue = finalValueOfGuess
-
-    return finalValueOfGuess
-
-
-#Subtract final estimate from the value of math.sqrt(x) to show how accurate your estimate is.
-math.sqrt(x)
-
-main
+class main():
+    #The program should print a short introduction about its purpose.
+        print('This program calculate the square root of a given number using the Newtons method')
+    #Prompt the user for the value xx to find x root(x).
+        x = input('What is the number for which you want to calculate the square root?')
+    #Prompt the user for the number of times to improve the guess.
+        n = input('How many iterations do you want to use?')
+    #Using Newtwon's method in order to estimate the square root
+        def newtownMethod(x,n):
+            guessValue = 0.5 * x
+            finalValueOfGuess = 0.5 * (guessValue+x/guessValue)
+            guessValue = finalValueOfGuess
+            return finalValueOfGuess
+    #The difference variable that stores the valie of the deffirence between my estimate and the actual result
+        difference = math.sqrt(x) - newtownMethod(x,n);
+        guess = newtownMethod(x,n)
+    #Printing the results
+        print('My guess for the square root of ',x,' is ',guess)
+        print('The difference between my guess and the real result is ',difference )
+    #Main method
+main()
